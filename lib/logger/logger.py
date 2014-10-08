0c28d70afc330adc40c770685a74af2acf28c32a
Mickroz <mike.kros@gmail.com>
2014-10-08 16:00:45 +0200

    Create logger.py

---
+++ lib/logger/logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#<derkens@gmail.com>
+#show cancelled? this script notifies you and put the show in 'pause' (after the last ep oc.)
+#
+# shamelessly incorporated stuff from Sick Beard AutoProcessTV
+# Author: Nic Wolfe <nic@wolfeden.ca>
+# URL: http://code.google.com/p/sickbeard/
+
+import logging, logging.config
+logging.config.fileConfig("logging.conf")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+							action="store_true")
+args = parser.parse_args()
+
+logger1 = logging.getLogger("logger1")
+logger2 = logging.getLogger("logger2")
+
+if args.verbose:
+	logging = logger2
+else:
+	logging = logger1
