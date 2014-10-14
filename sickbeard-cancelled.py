3bb829070e911e892b3edd241aa3b8254610f983
derkens <derkens@gmail.com>
2014-10-14 21:18:20 +0200

    update script to use config and logger imports

--- sickbeard-cancelled.py
+++ sickbeard-cancelled.py
@@ -10,151 +10,60 @@
 import os.path
 import sys
 import httplib, urllib, urllib2, json
-import logging, logging.config
-logging.config.fileConfig("lib/logger/logging.conf")
-import argparse
+import lib.logger.logger as logger
+import lib.config as config
+import lib.emailer as emailer
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity",
-							action="store_true")
-args = parser.parse_args()
-
-logger1 = logging.getLogger("logger1")
-logger2 = logging.getLogger("logger2")
-
-if args.verbose:
-	logging = logger2
-else:
-	logging = logger1
-# Try importing Python 2 modules using new names
-try:
-	import ConfigParser as configparser
-	import urllib2
-	from urllib import urlencode
-
-# On error import Python 3 modules
-except ImportError:
-	import configparser
-	import urllib.request as urllib2
-	from urllib.parse import urlencode
-
-# Default values
-host = "localhost"
-port = "8081"
-api_key = ""
-ssl = 0
-web_root = "/"
-app = "SickBeard"
-
-default_url = host + ":" + port + web_root
-if ssl:
-	default_url = "https://" + default_url
-else:
-	default_url = "http://" + default_url
-
-# Get values from config_file
-config = configparser.RawConfigParser()
-config_filename = os.path.join(os.path.dirname(sys.argv[0]), "settings.cfg")
-
-if not os.path.isfile(config_filename):
-	logging.error (config_filename + " doesn\'t exist")
-	logging.error ("copy /rename " + config_filename + ".sample and edit")
-	sys.exit(1)
-
-else:
-	try:
-		logging.info ("Loading config from " + config_filename)
-
-		with open(config_filename, "r") as fp:
-			config.readfp(fp)
-
-		# Replace default values with config_file values
-		host = config.get("SickBeard", "host")
-		port = config.get("SickBeard", "port")
-		api_key = config.get("SickBeard", "api_key")
-		lvl = config.get("General", "loglevel")
-		logger1.setLevel(lvl)
-
-		if not api_key:
-			logging.error ("Sick Beard api key setting is empty, please fill this field in settings.cfg")
-			sys.exit(1)
-
-		try:
-			ssl = int(config.get("SickBeard", "ssl"))
-			use_pushover = int(config.get("Pushover", "use_pushover"))
-			app_token = config.get("SickBeard", "app_token")
-			user_key = config.get("Pushover", "user_key")
-			use_nma = int(config.get("NMA", "use_nma"))
-			nma_api = config.get("NMA", "nma_api")
-			nma_priority = config.get("NMA", "nma_priority")
-
-		except (configparser.NoOptionError, ValueError):
-			pass
-
-		try:
-			web_root = config.get("SickBeard", "web_root")
-			if not web_root.startswith("/"):
-				web_root = "/" + web_root
-
-			if not web_root.endswith("/"):
-				web_root = web_root + "/"
-
-		except configparser.NoOptionError:
-			pass
-
-	except EnvironmentError:
-		e = sys.exc_info()[1]
-		logging.error ("Could not read configuration file: " + str(e))
-		# There was a config_file, don't use default values but exit
-		sys.exit(1)
-
-if ssl:
+if config.ssl:
 	protocol = "https://"
 else:
 	protocol = "http://"
 
-url = protocol + host + ":" + port + web_root + "api/" + api_key + "/?"
+if config.use_email == 1:
+	text_file = open("Output.txt", "w")
 
-logging.info ("Opening URL: " + url)
+url = protocol + config.host + ":" + config.port + config.web_root + "api/" + config.api_key + "/?"
 
-params = urlencode({'cmd': 'shows', 'sort': 'name', 'paused': '0'})
+logger.logging.info ("Opening URL: " + url)
+
+params = config.urlencode({'cmd': 'shows', 'sort': 'name', 'paused': '0'})
 t = urllib2.urlopen(url, params).read()
 t = json.loads(t)
-logging.debug(t)
+logger.logging.debug(t)
 end = filter( lambda x: x['status']=='Ended', t['data'].values() )
-logging.debug(end)
+logger.logging.debug(end)
 if end == []:
-	logging.info("Nothing to be done, exiting")
+	logger.logging.info("Nothing to be done, exiting")
 	exit()
 for i in end :
-	show = i['show_name'] ; logging.debug("Showname = " + show)
-	stat = i['status'] ; logging.debug("Show status = " + stat)
-	net = i['next_ep_airdate'] ; logging.debug("Next episode date = " + net)
+	show = i['show_name'] ; logger.logging.debug("Showname = " + show)
+	stat = i['status'] ; logger.logging.debug("Show status = " + stat)
+	net = i['next_ep_airdate'] ; logger.logging.debug("Next episode date = " + net)
 	if net == '':
-		params = urlencode({'cmd': 'sb.searchtvdb', 'lang': 'nl', 'name': show})
+		params = config.urlencode({'cmd': 'sb.searchtvdb', 'lang': 'nl', 'name': show})
 		r = urllib2.urlopen(url, params).read()
 		r = json.loads(r)
-		logging.debug(r)
+		logger.logging.debug(r)
 		tvdbid = str(r['data']['results'][0]['tvdbid'])
-		logging.debug(tvdbid)
-		params = urlencode({'cmd': 'show.pause', 'tvdbid': tvdbid, 'pause': 1})
+		logger.logging.debug(tvdbid)
+		params = config.urlencode({'cmd': 'show.pause', 'tvdbid': tvdbid, 'pause': 1})
 		s = urllib2.urlopen(url, params).read()
 		logging.debug(s)
-		if use_pushover == 1:
-			logging.info ("Sending Pushover notification...")
+		if config.use_pushover == 1:
+			logger.logging.info ("Sending Pushover notification...")
 			conn = httplib.HTTPSConnection("api.pushover.net:443")
 			conn.request("POST", "/1/messages.json",
 				urllib.urlencode({
-					"token": app_token,
-					"user": user_key,
+					"token": config.app_token,
+					"user": config.user_key,
 					"message": show+msgsuffix,
 					"title" : 'Sick Beard',
 				}), { "Content-type": "application/x-www-form-urlencoded" })
 			conn.getresponse()
 		if use_nma == 1:
-			logging.info ("Sending NMA notification...")
+			logger.logging.info ("Sending NMA notification...")
 			from lib.pynma import pynma
-			p = pynma.PyNMA(nma_api)
-			p.push(app, show+msgsuffix, show+msgsuffix, 0, 1, nma_priority )
+			p = pynma.PyNMA(config.nma_api)
+			p.push(config.app, show+msgsuffix, show+msgsuffix, 0, 1, config.nma_priority )
 
 
