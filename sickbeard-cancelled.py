4578ee7a261477c651e9189e2572ca58c302aaf7
derkens <derkens@gmail.com>
2014-10-15 09:37:43 +0200

    Revert "attempt 2"
    This reverts commit 711f8719a88bc141e66b4721c639e14e14667daa.

--- sickbeard-cancelled.py
+++ sickbeard-cancelled.py
@@ -56,12 +56,9 @@ for i in end :
 				urllib.urlencode({
 					"token": config.app_token,
 					"user": config.user_key,
-<<<<<<< HEAD
 					"message": show+" is possibly cancelled",
-=======
-					"message": show+msgsuffix,
->>>>>>> parent of e1c8835... small notification fixes
 					"title" : 'Sick Beard',
+					"device" : config.push_device,
 				}), { "Content-type": "application/x-www-form-urlencoded" })
 			r = conn.getresponse()
 			r = json.loads(r.read())
@@ -69,7 +66,7 @@ for i in end :
 				logger.logging.info("Pushover notification sent succesfully")
 			else:
 				logger.logging.error("Pushover failed with following error" + str(r["errors"]))
-		if use_nma == 1:
+		if config.use_nma == 1:
 			pushtitle = "SickBeard"
 			logger.logging.info ("Sending NMA notification...")
 			from lib.pynma import pynma
