26d06df4a850ab88f29df6c009a1b873bb97ac4e
Mickroz <mike.kros@gmail.com>
2015-06-06 14:15:02 +0200

    cleanup and fixes

--- tmToPVR.py
+++ tmToPVR.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python2
-import sys
-import os
-import time
-import ConfigParser
-import logging
-
-configFilename = "/volume1/scripts/autoProcessTV/autoProcessTV.cfg"
-
 import os.path
 import sys
 import os
@@ -27,28 +19,32 @@ def transmission():
 	
 def main():
 	logger.logging.info('Starting external PostProcess script ' + __file__)
+	
+	torrent_method = 'transmission'
+	if not torrent_method in ['utorrent', 'transmission', 'deluge', 'blackhole']:
+		logger.logging.error('Unknown Torrent Method. Aborting!')
+		time.sleep(3)
+		sys.exit()
 		
+	dirName, nzbName = eval(locals()['torrent_method'])()
+	
 	if not config.tv_dir:
 		logger.logging.error('Fill in [SickbBeard] tv_download_dir in settings.cfg to use this Script. Aborting!')
-		print 'Fill in [SickbBeard] tv_download_dir in settings.cfg to use this Script. Aborting!'
 		time.sleep(3)
 		sys.exit()
 
 	if dirName is None:
 		logger.logging.error('tmToPVR script need a dir to be run. Aborting!')
-		print 'MediaToSickbeard script need a dir to be run. Aborting!'
 		time.sleep(3)
 		sys.exit()
 
 	if "movies" in dirName:
 		logger.logging.error('Found movies in dirName. Aborting!')
-		print 'Found movies in dirName. Aborting!'
 		time.sleep(3)
 		sys.exit()
 
 	if not os.path.isdir(dirName):
 		logger.logging.error('Folder ' + dirName + ' does not exist. Aborting AutoPostProcess.')
-		print 'Folder ' + dirName + ' does not exist. Aborting AutoPostProcess.'
 		time.sleep(3)
 		sys.exit()
 
@@ -67,12 +63,11 @@ def main():
 	login_url = config.protocol + config.host + ":" + config.port + config.web_root + "/login"
 	
 	logger.logging.debug("Opening URL: " + url + ' with params=' + str(params))   
-	print "Opening URL: " + url + ' with params=' + str(params)
 	
 	try:
 		sess = requests.Session()
-		sess.post(login_url, data={'username': username, 'password': password}, stream=True, verify=False)
-		response = sess.get(url, auth=(username, password), params=params, verify=False,  allow_redirects=False)
+		sess.post(login_url, data={'username': config.username, 'password': config.password}, stream=True, verify=False)
+		response = sess.get(url, auth=(config.username, config.password), params=params, verify=False,  allow_redirects=False)
 	except Exception, e:
 		logger.logging.error(': Unknown exception raised when opening url: ' + str(e))
 		time.sleep(3)
@@ -80,13 +75,11 @@ def main():
 	
 	if response.status_code == 302:
 		logger.logging.error('Invalid Sickbeard Username or Password, check your config')
-		print 'Invalid Sickbeard Username or Password, check your config'
 		time.sleep(3)
 		sys.exit()
 	
 	if response.status_code == 200:
 		logger.logging.info('Script ' + __file__ + ' Succesfull')
-		print 'Script ' + __file__ + ' Succesfull'
 		time.sleep(3)
 		sys.exit()
 		
