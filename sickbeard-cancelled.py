b246ba374690aec941edc60daa05ef9e3d7f1f59
derkens <derkens@gmail.com>
2016-04-26 09:41:17 +0200

    #8 partial rewrite, do not pause shows and limit sick(rage) functionality (propers etc.)

--- sickbeard-cancelled.py
+++ sickbeard-cancelled.py
@@ -31,9 +31,9 @@ logger.logging.debug(json.dumps(end, indent=4))
 if end == []:
 	logger.logging.info("Nothing to be done, exiting")
 
-for i in end:
-	showname = i['show_name']; logger.logging.debug("Showname = " + showname)
-	sickid = i['indexerid']
+for i, value in enumerate(end):
+	showname = end[i]['show_name']; logger.logging.debug("Showname = " + showname)
+	sickid = end[i]['indexerid']
 	params = {'cmd': 'show.stats', 'indexerid': sickid}
 	res = api.sick_call(params)
 	total = res['data']['total']
@@ -41,7 +41,7 @@ for i in end:
 	totalsnt = res['data']['snatched']['total']
 	unaired = res['data']['unaired']
 
-	stat = i['status']; logger.logging.debug("Show status = " + stat)
+	stat = end[i]['status']; logger.logging.debug("Show status = " + stat)
 	logger.logging.info(showname + " has status: " + stat)
 	if unaired > 0:
 		logger.logging.info(showname + " has status Ended, but not all episodes have aired.")
@@ -49,31 +49,37 @@ for i in end:
 		logger.logging.info(showname + " has status Ended, but not all episodes are downloaded")
 	if unaired == 0 and (total - totaldwl == 0) and totalsnt == 0:
 		logger.logging.debug(showname + " has " + indexer + " " + str(sickid))
-		params = {'cmd': 'show.pause', indexer: sickid, 'pause': 1}
-		res = api.sick_call(params)
+		sbcancelled = misc.openpick('sb-cancelled')
+		if sbcancelled == "novalue":
+			sbcancelled = []
+		if str(sickid) not in sbcancelled:
+			sbcancelled.append(str(sickid))
+			misc.dumppick('sb-cancelled', sbcancelled)
 
-		args = {'showname': showname.encode("utf-8")}
-		pushtitle, pushmsg = misc.replace(config.sbca_push_title, config.sbca_push_msg, **args)
+			args = {'showname': showname.encode("utf-8")}
+			pushtitle, pushmsg = misc.replace(config.sbca_push_title, config.sbca_push_msg, **args)
 
-		if config.use_pushover:
-			push_info = {'potitle': pushtitle, 'pomsg': pushmsg, 'sound': config.sbca_push_sound}
-			api.pushover(config.user_key, config.app_token, config.push_device, **push_info)
-		if config.use_nma:
-			logger.logging.debug("Sending NMA notification...")
-			from lib.pynma import pynma
-			p = pynma.PyNMA(config.nma_api)
-			res = p.push(config.app, pushtitle, pushmsg, 0, 1, config.nma_priority)
-			if res[config.nma_api][u'code'] == u'200':
-				logger.logging.info("NMA Notification succesfully send")
-			else:
-				error = res[config.nma_api]['message'].encode('ascii')
-				logger.logging.error("NMA Notification failed: " + error)
-		if config.use_pushbullet:
-			push_info = pushtitle, pushmsg, config.deviceid, config.channeltag
-			api.pushbullet(push_info)
-		if config.use_email:
-			text_file.write(show + " is possibly cancelled\n")
-
+			if config.use_pushover:
+				push_info = {'potitle': pushtitle, 'pomsg': pushmsg, 'sound': config.sbca_push_sound}
+				api.pushover(config.user_key, config.app_token, config.push_device, **push_info)
+			if config.use_nma:
+				logger.logging.debug("Sending NMA notification...")
+				from lib.pynma import pynma
+				p = pynma.PyNMA(config.nma_api)
+				res = p.push(config.app, pushtitle, pushmsg, 0, 1, config.nma_priority)
+				if res[config.nma_api][u'code'] == u'200':
+					logger.logging.info("NMA Notification succesfully send")
+				else:
+					error = res[config.nma_api]['message'].encode('ascii')
+					logger.logging.error("NMA Notification failed: " + error)
+			if config.use_pushbullet:
+				push_info = pushtitle, pushmsg, config.deviceid, config.channeltag
+				api.pushbullet(push_info)
+			if config.use_email:
+				text_file.write(show + " is possibly cancelled\n")
+		else:
+			logger.logging.debug(str(sickid) + " is already in pickle, skipping")
+			logger.logging.info("Nothing to be done, exiting")
 
 if config.use_email:
 	text_file.close()
